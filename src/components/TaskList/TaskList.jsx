f79c2258dac822f371f18242c5fdb110ee2df7fb
KH Bidyut <khbidyut31@gmail.com>
2026-06-23 22:13:03 +0600

    feat: implement EmployeeDashboard with Header, TaskListNumbers, and TaskList components

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,51 @@
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="h-[55%] overflow-x-auto overflow-y-hidden flex items-center gap-5 flex-nowrap w-full py-5 mt-10"
+    >
+      <div className="shrink-0 h-full w-75 p-5 rounded-xl bg-red-400">
+        <div className="flex justify-between items-center">
+            <h3 className="bg-red-600 px-3 text-sm py-1">High</h3>
+            <h4 className="text-sm">23 Jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold ">Make a youtube video </h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis vel aspernatur facilis architecto distinctio!</p>
+      </div>
+      <div className="shrink-0 h-full w-75 p-5 rounded-xl bg-green-400">
+        <div className="flex justify-between items-center">
+            <h3 className="bg-red-600 px-3 text-sm py-1">High</h3>
+            <h4 className="text-sm">23 Jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold ">Make a youtube video </h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis vel aspernatur facilis architecto distinctio!</p>
+      </div>
+      <div className="shrink-0 h-full w-75 p-5 rounded-xl bg-blue-400">
+        <div className="flex justify-between items-center">
+            <h3 className="bg-red-600 px-3 text-sm py-1">High</h3>
+            <h4 className="text-sm">23 Jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold ">Make a youtube video </h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis vel aspernatur facilis architecto distinctio!</p>
+      </div>
+      <div className="shrink-0 h-full w-75 p-5 rounded-xl bg-pink-400">
+        <div className="flex justify-between items-center">
+            <h3 className="bg-red-600 px-3 text-sm py-1">High</h3>
+            <h4 className="text-sm">23 Jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold ">Make a youtube video </h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis vel aspernatur facilis architecto distinctio!</p>
+      </div>
+      <div className="shrink-0 h-full w-75 p-5 rounded-xl bg-yellow-400">
+        <div className="flex justify-between items-center">
+            <h3 className="bg-red-600 px-3 text-sm py-1">High</h3>
+            <h4 className="text-sm">23 Jun 2026</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold ">Make a youtube video </h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis vel aspernatur facilis architecto distinctio!</p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
---
+++ src/components/other/TaskListNumbers.jsx
@@ -0,0 +1,27 @@
+const TaskListNumbers = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-10 w-full">
+      <div className="rounded-xl py-6 px-9 bg-red-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+
+      <div className="rounded-xl py-6 px-9 bg-blue-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">Completed Task</h3>
+      </div>
+
+      <div className="rounded-xl py-6 px-9 bg-green-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">Accepted Task</h3>
+      </div>
+
+      <div className="rounded-xl py-6 px-9 bg-yellow-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">Failed Task</h3>
+      </div>
+    </div>
+  );
+};
+
+export default TaskListNumbers;
